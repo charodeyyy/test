0198f83f0e62e6f6eff4fe7d920f8d5e6c3d1178
s.kharchenko <s.kharchenko@smartweb.com.ua>
2017-07-30 16:43:04 +0300

    try

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default.conf
 sudo /etc/init.d/nginx restart
 # sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test.conf
-/home/box/web sudo gunicorn - b 0.0.0.0:8080 hello:app
-sudo /etc/init.d/gunicorn restart
+# /home/box/web sudo gunicorn - b 0.0.0.0:8080 hello:app
+# sudo /etc/init.d/gunicorn restart
