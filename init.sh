a36a421588beaf88862f962deed60beb0b710b2d
s.kharchenko <s.kharchenko@smartweb.com.ua>
2017-08-02 00:36:13 +0300

    nginx

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
-sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default.conf
 sudo /etc/init.d/nginx restart
 # sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test.conf
 # /home/box/web sudo gunicorn - b 0.0.0.0:8080 hello:app
