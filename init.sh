b14f3c03506b3cbb385a04a6b1d2342f3a5ca358
charodeyyy <slava.charodey@gmail.com>
2017-08-15 01:27:02 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 sudo ln -sf /home/box/web/etc/default.conf /etc/nginx/sites-enabled/default.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test.conf
+sudo ln -sf /home/box/web/etc/gunicorn1.conf /etc/gunicorn.d/test.conf
 cd /home/box/web/ask
-#sudo gunicorn -b 0.0.0.0:8080 hello:hello
-sudo gunicorn -c /home/box/web/etc/gunicorn1.conf ask.wsgi:application
+sudo gunicorn -b 0.0.0.0:8080 ask.wsgi:application
+#sudo gunicorn -c /home/box/web/etc/gunicorn1.conf ask.wsgi:application
 #sudo /etc/init.d/gunicorn restart
