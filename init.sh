e0abc955579887f9501b109f98cce41f6771c7cb
charodeyyy <slava.charodey@gmail.com>
2017-08-15 01:13:12 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test.conf
 cd /home/box/web/ask
 #sudo gunicorn -b 0.0.0.0:8080 hello:hello
-sudo gunicorn -c /home/sl/web/etc/gunicorn1.conf ask.wsgi:application
+sudo gunicorn -c /home/box/web/etc/gunicorn1.conf ask.wsgi:application
 #sudo /etc/init.d/gunicorn restart
