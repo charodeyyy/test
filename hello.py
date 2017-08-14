b08351e84ae95697df9e76aa5b0b9c6dfc34d625
s.kharchenko <s.kharchenko@smartweb.com.ua>
2017-08-15 01:09:55 +0300

    django_project

--- hello.py
+++ hello.py
@@ -3,8 +3,6 @@ def hello(environ, start_response):
   status = '200 OK'
   response_headers = [('Content-type','text/plain')]
   start_response(status, response_headers)
-  # resp = environ.get('QUERY_STRING').split("&")
-  # resp = [item+"\n" for item in resp]
   resp = "\n".join(environ.get('QUERY_STRING').split("&"))
-  print(resp)
-  return resp
+  return [str.encode(resp)]
+  
