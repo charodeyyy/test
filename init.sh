181f9c2578ef63f03902801f2fd1f5152397d49f
s.kharchenko <s.kharchenko@smartweb.com.ua>
2017-08-15 01:09:55 +0300

    django_project

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
-git clone https://github.com/charodeyyy/web.git
 sudo ln -sf /home/box/web/etc/default.conf /etc/nginx/sites-enabled/default.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test.conf
-# /home/box/web sudo gunicorn - b 0.0.0.0:8080 hello:app
-sudo /etc/init.d/gunicorn restart
+cd /home/box/web/ask
+#sudo gunicorn -b 0.0.0.0:8080 hello:hello
+sudo gunicorn -c /home/sl/web/etc/gunicorn1.conf ask.wsgi:application
+#sudo /etc/init.d/gunicorn restart
